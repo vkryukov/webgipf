c0797d7c754c8073bd8b6eecc2fcd4025b768fe1
Victor Kryukov <victor@kryukov.me>
2023-11-05 00:26:51 -0700

    Add neighbors test

--- client/tests/GipfTests.elm
+++ client/tests/GipfTests.elm
@@ -5,6 +5,20 @@ import Gipf
 import Test exposing (..)
 
 
+sortByXY : List Gipf.Coord -> List Gipf.Coord
+sortByXY coords =
+    coords
+        |> List.sortBy .y
+        |> List.sortBy .x
+
+
+neighborsTest : String -> Gipf.Coord -> List Gipf.Coord -> Test
+neighborsTest description point expectedNeighbors =
+    test description <|
+        \_ ->
+            Expect.equal (sortByXY expectedNeighbors) (sortByXY (Gipf.neighbors point))
+
+
 all : Test
 all =
     describe "Gipf Tests"
@@ -33,18 +47,33 @@ all =
                     Expect.equal False (Gipf.edgeBoardPointQ { x = 4, y = 4 })
             ]
         , describe "neighbors function"
-            [ test "finds neighbors of a point" <|
-                \_ ->
-                    let
-                        expectedNeighbors =
-                            [ { x = 4, y = 5 }
-                            , { x = 4, y = 3 }
-                            , { x = 5, y = 4 }
-                            , { x = 3, y = 4 }
-                            , { x = 5, y = 5 }
-                            , { x = 3, y = 3 }
-                            ]
-                    in
-                    Expect.equal expectedNeighbors (Gipf.neighbors { x = 4, y = 4 })
+            [ neighborsTest "finds neighbors of a point at (4, 4)"
+                { x = 4, y = 4 }
+                [ { x = 4, y = 5 }
+                , { x = 4, y = 3 }
+                , { x = 5, y = 4 }
+                , { x = 3, y = 4 }
+                , { x = 5, y = 5 }
+                , { x = 3, y = 3 }
+                ]
+            , neighborsTest "finds neighbors of a point at (0, 0)"
+                { x = 0, y = 0 }
+                [ { x = 1, y = 1 }
+                ]
+            , neighborsTest "finds neighbors of a point at (0, 4)"
+                { x = 0, y = 4 }
+                [ { x = 1, y = 4 }
+                ]
+            , neighborsTest "finds neighbors of a point at (1, 0)"
+                { x = 1, y = 0 }
+                [ { x = 1, y = 1 }
+                , { x = 2, y = 1 }
+                ]
+            , neighborsTest "finds neighbors of a point at (7, 7)"
+                { x = 7, y = 7 }
+                [ { x = 6, y = 7 }
+                , { x = 7, y = 6 }
+                , { x = 6, y = 6 }
+                ]
             ]
         ]
