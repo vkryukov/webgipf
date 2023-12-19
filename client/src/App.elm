451dd325549e6f8a98d5b2d248be33ed39b47001
Victor Kryukov <victor.kryukov@gmail.com>
2023-12-18 21:47:16 -0800

    new game creation form

--- client/src/App.elm
+++ client/src/App.elm
@@ -1,11 +1,13 @@
-module App exposing (Model, Msg(..), init, main, subscriptions, update, view, viewLink)
+module App exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Auth
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onCheck, onClick, onInput)
 import Json.Encode as Encode
+import Ui exposing (viewPrimaryButton)
 import Url
 
 
@@ -33,6 +35,8 @@ type alias Model =
     { key : Nav.Key
     , url : Url.Url
     , auth : Auth.Model
+    , gameType : String
+    , color : String
     }
 
 
@@ -42,7 +46,8 @@ init flags url key =
         ( auth, authCmd ) =
             Auth.init flags
     in
-    ( Model key url auth, Cmd.map AuthMsg authCmd )
+    -- TODO: Default gameType should be in sync with the select choices
+    ( Model key url auth "Basic GIPF" "white", Cmd.map AuthMsg authCmd )
 
 
 
@@ -53,6 +58,10 @@ type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
     | AuthMsg Auth.Msg
+    | SelectGameType String
+    | SelectColor String
+    | CreateGame
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -78,6 +87,22 @@ update msg model =
             in
             ( { model | auth = auth }, Cmd.map AuthMsg authCmd )
 
+        SelectGameType gameType ->
+            ( { model | gameType = gameType }, Cmd.none )
+
+        SelectColor color ->
+            ( { model | color = color }, Cmd.none )
+
+        CreateGame ->
+            let
+                _ =
+                    Debug.log "Create game" ( model.gameType, model.color )
+            in
+            ( model, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
@@ -96,25 +121,83 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Project Gipf"
     , body =
-        Html.map AuthMsg (Auth.view model.auth)
-            :: viewLinks model
+        [ Html.map AuthMsg (Auth.view model.auth)
+        , viewCreateNewGame
+        ]
+
+    -- :: viewLinks model
     }
 
 
-viewLinks : Model -> List (Html msg)
-viewLinks model =
-    [ text "The current URL is: "
-    , b [] [ text (Url.toString model.url) ]
-    , ul []
-        [ viewLink "/home"
-        , viewLink "/profile"
-        , viewLink "/reviews/the-century-of-the-self"
-        , viewLink "/reviews/public-opinion"
-        , viewLink "/reviews/shah-of-shahs"
+viewCreateNewGame : Html Msg
+viewCreateNewGame =
+    div [ class "p-4" ]
+        [ h2 [ class "text-lg font-bold mb-4" ] [ text "Create new game" ]
+        , div [ class "flex items-center space-x-4" ]
+            [ label [ class "mr-2" ] [ text "Select game type:" ]
+            , select [ class "form-select", onInput SelectGameType ]
+                [ option [] [ text "Basic GIPF" ]
+                , option [] [ text "Standard GIPF" ]
+                , option [] [ text "Tournament GIPF" ]
+                ]
+            , label [ class "ml-4 mr-2" ] [ text "Play as:" ]
+            , div [ class "flex items-center space-x-4" ]
+                [ label []
+                    [ input
+                        [ type_ "radio"
+                        , name "choice"
+                        , value "white"
+                        , class "form-radio"
+                        , checked True
+                        , onCheck
+                            (\isChecked ->
+                                if isChecked then
+                                    SelectColor "white"
+
+                                else
+                                    NoOp
+                            )
+                        ]
+                        []
+                    , text " White"
+                    ]
+                , label []
+                    [ input
+                        [ type_ "radio"
+                        , name "choice"
+                        , value "black"
+                        , class "form-radio"
+                        , onCheck
+                            (\isChecked ->
+                                if isChecked then
+                                    SelectColor "black"
+
+                                else
+                                    NoOp
+                            )
+                        ]
+                        []
+                    , text " Black"
+                    ]
+                ]
+            , viewPrimaryButton ( "Create game", CreateGame )
+            ]
         ]
-    ]
 
 
-viewLink : String -> Html msg
-viewLink path =
-    li [] [ a [ href path ] [ text path ] ]
+
+-- viewLinks : Model -> List (Html msg)
+-- viewLinks model =
+--     [ text "The current URL is: "
+--     , b [] [ text (Url.toString model.url) ]
+--     , ul []
+--         [ viewLink "/home"
+--         , viewLink "/profile"
+--         , viewLink "/reviews/the-century-of-the-self"
+--         , viewLink "/reviews/public-opinion"
+--         , viewLink "/reviews/shah-of-shahs"
+--         ]
+--     ]
+-- viewLink : String -> Html msg
+-- viewLink path =
+--     li [] [ a [ href path ] [ text path ] ]
